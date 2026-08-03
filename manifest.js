debc400b68635b502af73b4ff9857b9c859e9e48
amanzewdu4117-dev <amanzewdu4117@gmail.com>
2026-08-03 22:02:23 +0300

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,15 @@
+{
+  "name": "Cozy's Wear",
+  "short_name": "Cozy Wear",
+  "start_url": "/index.html",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#8b4513",
+  "icons": [
+    {
+      "src": "icon.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
